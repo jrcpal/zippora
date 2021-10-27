763fa639180c10b8612d88a808d67a5b9b19866c
jrcpal <jchurgy@gmail.com>
2021-10-27 10:53:25 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@
 
 const app = require("./app.js");
 const { PORT } = require("./config");
-
+app.get("/", (res,req) => {
+  res.send({"message":"Hello?"})
+})
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
