8c142ac8cc32ac940fe2cc1fe7a24b164eb979c9
jrcpal <jchurgy@gmail.com>
2021-10-27 10:56:58 -0400

    updates to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 
 const app = require("./app.js");
 const { PORT } = require("./config");
+app.use(express.static("../frontend/build/index.html"))
 app.get("/", (res,req) => {
   res.send({"message":"Hello?"})
 })
